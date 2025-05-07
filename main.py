b87ad51fe869ec642e3eb5fe03cef5436af470c8
Kr-Verified <dmschd0573@gmail.com>
2025-05-07 20:18:37 +0900

    변경3

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import streamlit as st
 
 col1, col2 = st.columns([2, 3])
-
-with col1:
+tab1, tab2 = st.tabs(['Tab A', 'Tab B'])
+with col1 :
   st.title("Here is column1 title")
-with col2:
+  with tab1 :
+    st.write('hello')
+  with tab2 :
+    st.write("hi")
+with col2 :
   st.title("here is column2 title")
   st.checkbox("this is checkbox1 in col2")
 
